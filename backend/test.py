6682961f3e3cd06ee1efee482423bd175d8f71ac
Ítalo Botelho <fragabotelhoitalo@gmail.com>
2026-06-06 16:07:55 -0300

    chore: Remove obsolete test scripts from backend

--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from pymongo import AsyncMongoClient
-
-async def main():
-    client = AsyncMongoClient('mongodb://localhost:27017')
-    db = client['epidemiologia_db']
-    doc = await db.casos_geolocalizados.find_one()
-    print([k for k in doc.keys()])
-asyncio.run(main())
